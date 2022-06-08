d37b7bb037f7f6f451251a3c9e0f5df48373b083
Player Ghost <sousaitalo12@gmail.com>
2022-06-08 08:30:34 -0300

    data treatment finalized

--- SendResultsToGoogleSheetsTool/main.py
+++ SendResultsToGoogleSheetsTool/main.py
@@ -17,7 +17,7 @@ def getInsertFromRawData(dbName):
 
                 if jAux[0].strip() in keywords or jAux[1].strip() in keywords:
                     if "Operations" not in jAux[1].strip() and "Return" not in jAux[1].strip():
-                        insert.append(jAux[2].strip())
+                        insert.append(jAux[2].strip().replace("'", ""))
 
     return insert
 
@@ -36,7 +36,7 @@ def getReadFromRawData(dbName):
 
                 if jAux[0].strip() in keywords or jAux[1].strip() in keywords:
                     if "Operations" not in jAux[1].strip() and "Return" not in jAux[1].strip():
-                        read.append(jAux[2].strip())
+                        read.append(jAux[2].strip().replace("'", ""))
 
     return read
 
@@ -55,7 +55,7 @@ def getReadHybridFromRawData(dbName):
 
                 if jAux[0].strip() in keywords or jAux[1].strip() in keywords:
                     if "Operations" not in jAux[1].strip() and "Return" not in jAux[1].strip():
-                        readHybrid.append(jAux[2].strip())
+                        readHybrid.append(jAux[2].strip().replace("'", ""))
 
     return readHybrid
 
@@ -74,7 +74,7 @@ def getUpdateHybridFromRawData(dbName):
 
                 if jAux[0].strip() in keywords or jAux[1].strip() in keywords:
                     if "Operations" not in jAux[1].strip() and "Return" not in jAux[1].strip():
-                        updateHybrid.append(jAux[2].strip())
+                        updateHybrid.append(jAux[2].strip().replace("'", ""))
 
     return updateHybrid
 
@@ -88,48 +88,38 @@ def LoadAndTreatData(dbName):
     return insert, read, insertHybrid, updateHybrid
 
 
-def updateSheet(sheet, metrics, startLine, endLine, isNewMetric=False):
-    minValues = []
-    maxValues = []
-    meanValues = []
-    medianValues = []
-    percentileValues = []
+def numpyMatrixToListMatrix(numpyMatrix):
+    listMatrix = []
 
-    for i in metrics:
-        minValues.append(i['min'])
-        maxValues.append(i['max'])
-        meanValues.append(i['avg'])
-        medianValues.append(i['med'])
-        percentileValues.append(i['p(95)'])
+    for i in range(len(numpyMatrix)):
+        listMatrix.append(numpyMatrix[i].tolist())
 
-    minValues = np.array_split(minValues, 3)
-    maxValues = np.array_split(maxValues, 3)
-    meanValues = np.array_split(meanValues, 3)
-    medianValues = np.array_split(medianValues, 3)
-    percentileValues = np.array_split(percentileValues, 3)
+    return listMatrix
 
-    columnsValues = []
 
-    for i in range(3):
-        columnValues = []
+def updateSheet(sheet, data, startLine, endLine, columnLabel):
+    metricRows = numpyMatrixToListMatrix(np.array_split(data, 21))
 
-        columnValues += minValues[i].tolist()
-        columnValues += maxValues[i].tolist()
-        columnValues += meanValues[i].tolist()
-        columnValues += medianValues[i].tolist()
-        columnValues += percentileValues[i].tolist()
+    startLineIndex = 4
+    endLineIndex = 7
+    metricCount = 0
 
-        columnsValues.append(columnValues)
-
-    columnsLabels = ["C", "D", "E"]
-
-    for i in range(len(columnsLabels)):
-        cellList = sheet.range(f'{columnsLabels[i]}{startLine}:{columnsLabels[i]}{endLine}')
+    for i in range(len(metricRows)):
+        cellList = sheet.range(f'{columnLabel}{startLineIndex}:{columnLabel}{endLineIndex}')
 
         for j in range(len(cellList)):
-            cellList[j].value = columnsValues[i][j]
+            cellList[j].value = float(metricRows[i][j])
 
         sheet.update_cells(cellList)
+
+        if metricCount < 2:
+            startLineIndex += 7
+            endLineIndex += 7
+            metricCount += 1
+        else:
+            startLineIndex += 8
+            endLineIndex += 8
+            metricCount = 0
 
 
 def loadResultsTxt(fileName):
@@ -139,11 +129,70 @@ def loadResultsTxt(fileName):
         return file.readlines()
 
 
+def getAllInserts(databasesData):
+    inserts = []
+
+    for i in range(0, 7):
+        for j in range(0, len(databasesData[0][0]), 7):
+            inserts.append(databasesData[0][0][j+i])
+            inserts.append(databasesData[1][0][j+i])
+            inserts.append(databasesData[2][0][j+i])
+            inserts.append(databasesData[3][0][j+i])
+
+    return inserts
+
+
+def getAllReads(databasesData):
+    read = []
+
+    for i in range(0, 7):
+        for j in range(0, len(databasesData[0][1]), 7):
+            read.append(databasesData[0][1][j + i])
+            read.append(databasesData[1][1][j + i])
+            read.append(databasesData[2][1][j + i])
+            read.append(databasesData[3][1][j + i])
+
+    return read
+
+
+def getAllInsertsHybrid(databasesData):
+    insertsHybrid = []
+
+    for i in range(0, 7):
+        for j in range(0, len(databasesData[0][2]), 7):
+            insertsHybrid.append(databasesData[0][2][j + i])
+            insertsHybrid.append(databasesData[1][2][j + i])
+            insertsHybrid.append(databasesData[2][2][j + i])
+            insertsHybrid.append(databasesData[3][2][j + i])
+
+    return insertsHybrid
+
+
+def getAllUpdatesHybrid(databasesData):
+    updatesHybrid = []
+
+    for i in range(0, 7):
+        for j in range(0, len(databasesData[0][3]), 7):
+            updatesHybrid.append(databasesData[0][3][j + i])
+            updatesHybrid.append(databasesData[1][3][j + i])
+            updatesHybrid.append(databasesData[2][3][j + i])
+            updatesHybrid.append(databasesData[3][3][j + i])
+
+    return updatesHybrid
+
+
 if __name__ == '__main__':
     mariaDBData = LoadAndTreatData("mariadb")
     mongoDB = LoadAndTreatData("mongodb")
-    #postGreSql = LoadAndTreatData("postgresql")
+    postGreSql = LoadAndTreatData("postgresql")
     redis = LoadAndTreatData("redis")
+
+    databasesData = [mariaDBData, mongoDB, postGreSql, redis]
+
+    insert = getAllInserts(databasesData)
+    read = getAllReads(databasesData)
+    insertHybrid = getAllInsertsHybrid(databasesData)
+    updateHybrid = getAllUpdatesHybrid(databasesData)
 
     # define the scope
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
@@ -155,6 +204,16 @@ if __name__ == '__main__':
     client = gspread.authorize(creds)
 
     # get the instance of the Spreadsheet
-    sheet = client.open_by_key("1_nZLaWe0RleL9vHWVRra3ltOy37VnQJy5SRqrdJNw9c").sheet1
+    sheet = client.open_by_key("142EyP1_sl6gJszKFme9MABFU_yDuJlJLaUJgVAqIMYU").sheet1
 
-    # updateSheet(sheet, metricsK6, 3, 17)
+    #updateSheet(sheet, insert, 4, 153, "B")
+    #updateSheet(sheet, read, 4, 153, "C")
+    #updateSheet(sheet, insertHybrid, 4, 153, "D")
+    updateSheet(sheet, updateHybrid, 4, 153, "E")
+
+    # cellList = sheet.range(f'k{10}:k{15}')
+    #
+    # for j in range(len(cellList)):
+    #     cellList[j].value = 10
+    #
+    # sheet.update_cells(cellList)
